aaf8d22e02c968b221e69224b083a38c19f0f967
Laure-C <55760660+Laure-C@users.noreply.github.com>
2022-10-04 22:27:54 +0200

    2 files modified

--- src/components/ThirdScreen.tsx
+++ src/components/ThirdScreen.tsx
@@ -17,8 +17,11 @@ export function ThirdScreen({ navigation }: ThirdScreenProps) {
   const grenobleWeather = WeatherByCityService.getWeatherByCityName('Grenoble');
   return (
     <flexboxLayout style={styles.container}>
-      <label text="Voici la météo de grenoble! " />
-      <image src={LinksIconWeather['cloudy']} />
+      <label class="titre">Météo de {grenobleWeather.cityName}</label>
+      <image
+        src={LinksIconWeather[grenobleWeather.forecastWeather?.currently?.icon]}
+      />
+      <label>{grenobleWeather.forecastWeather?.currently?.summary}</label>
       <button onTap={() => navigation.goBack()}>Retour Menu</button>
     </flexboxLayout>
   );
